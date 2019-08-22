cb700387ea57b629b1dac8a11aaad24a26500a58
Jens Axel Søgaard <jensaxel@soegaard.net>
2019-08-22 17:52:47 +0200

    Added a multi info file

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang info
+(define collection 'multi)
+(define version    "0.1")
+(define deps       '("base" "pict-lib"))
+(define build-deps '( "scribble-lib"
+                      "racket-doc"
+                      "draw-doc"
+                      "pict-doc"))
+
+
