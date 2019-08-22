05e3409d1294ed061474dc7de41f1ed58cd41bed
Jens Axel Søgaard <jensaxel@soegaard.net>
2019-08-22 17:12:24 +0200

    Initial commit

---
+++ latex-pict/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "latex-pict")
+(define deps '("base" "pict" "racket-poppler" "racket/format" "racket/match"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/latex-pict.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(soegaard))
